7efc1cf0e8ca788c0a11b18dcc242fcc6485df90
renatopessoa <renato.fjn.si@gmail.com>
2026-03-26 15:13:42 -0300

    ajuste any

--- app/actions.ts
+++ app/actions.ts
@@ -10,7 +10,7 @@ import { authOptions } from "@/lib/auth";
 async function getUserId() {
   try {
     const session = await getServerSession(authOptions);
-    return (session?.user?.id as any)?.id;
+    return (session?.user as any)?.id;
   } catch (error) {
     console.error("Erro ao recuperar sessão:", error);
     return undefined;
